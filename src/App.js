fbeb42d59fea186f9b3ec8038b75e7ae6c8a9c3c
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-26 16:46:40 +0200

    adding state to app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState}from 'react'
+import './index.css'
+
 const App=()=>{
+  const [counter,setCounter]=useState(0)
+  const [value,setValues]=useState([])
+  const handleClick=()=>{
+    setCounter(counter+1)
+    setValues(value.concat(counter))
+  }
+
   return(
-   <div>
+   <div className='container'>
+     <p>React App configuration using webpack </p>
+     <p>webpack {counter} clicks</p>
+     <button onClick={handleClick}>press</button><br/> to change counter
      
-     <a href='https://www.youtube.com/watch?v=IRnqDETxOUc'>Timro vaye fan</a>
+     
    </div>
   )
  }
