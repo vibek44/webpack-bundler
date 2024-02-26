1d8b366d0aab07654ba125832c0838cd99b026ef
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-26 19:02:22 +0200

    adding code to verify configuration setting is workig

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useState}from 'react'
+import {useNotes} from './hooks/index'
 import './index.css'
 
 const App=()=>{
   const [counter,setCounter]=useState(0)
   const [value,setValues]=useState([])
+  const notes=useNotes(BACKEND_URL)
   const handleClick=()=>{
     setCounter(counter+1)
     setValues(value.concat(counter))
@@ -14,8 +16,9 @@ const App=()=>{
      <p>React App configuration using webpack </p>
      <p>webpack {counter} clicks</p>
      <button onClick={handleClick}>press</button><br/> to change counter
-     
-     
+     <p>notes has {notes.length} length</p>
+
+    
    </div>
   )
  }
