c6ed6a57898df7dedea4696d27cf21e9418b318e
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-26 16:51:19 +0200

    adding  rules for css and devtool:sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,28 @@ const config=()=>{
       path:path.resolve(__dirname, 'build'),
       filename:'main.js'
     },
+    devServer:{  //devserver to create localhost server
+      static:path.resolve(__dirname, 'build'),
+      compress:true,
+      port:3000
+    },
+    devtool:'source-map',//use only for development
     module:{
-      rules:[
+      rules:[  // rules to allow jsx to plain javascript
         {
           test:/\.js$/,
           loader:'babel-loader',
           options:{
-            presets:['@babel/preset-react']
+            presets:[ '@babel/preset-env','@babel/preset-react']
           }
           
+        },
+        {    //css loader to load css file and style loader to inject and create element using css
+          test:/\.css$/,
+          use:['style-loader', 'css-loader']
+
         }
-      ]
+      ],
     }
 
   }
